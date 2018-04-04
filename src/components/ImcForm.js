32b3815a89afbee7691c85d87abf6d26a8553ad2
Bruno Paiva <bruno.paiva82@sapo.pt>
2018-04-04 17:54:45 +0100

    Correct formatting

--- src/components/ImcForm.js
+++ src/components/ImcForm.js
@@ -12,32 +12,32 @@ class ImcForm extends React.Component {
       weight: 0,
       warning: false
     };
-    
+
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
-  
+
   handleInputChange(event) {
     const name = event.target.name;
     this.setState({
       [name]: event.target.value
     });
   }
-  
+
   handleClick(event) {
     event.preventDefault();
-    
+
     const weight = this.state.weight;
     const height = this.state.height;
-    
+
     if (height && weight) {
       const imc = (weight / (height * height)).toFixed(2);
       this.setState({result: imc, warning: false});
     } else {
-      this.setState({result: null, warning: true}); 
+      this.setState({result: null, warning: true});
     }
   }
-  
+
   render() {
     return (
       <form className="form" role="form">
@@ -47,12 +47,12 @@ class ImcForm extends React.Component {
             <ImcWarning warning={this.state.warning} />
           </div>
         </div>
-        <NumberInput 
+        <NumberInput
           name="height"
           label="Height(meters)"
           placeholder={0}
-          onChange={this.handleInputChange} />  
-        <NumberInput 
+          onChange={this.handleInputChange} />
+        <NumberInput
           name="weight"
           label="Weight(kg)"
           placeholder={0}
